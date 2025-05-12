d76dc5add816fd9260222e76f91cf35ecb30156d
eric <greatbooms109@gmail.com>
2025-05-12 22:03:19 +0900

    Fix: 코인 계좌 번호 필수 제거

--- src/coin-summary/dto/create-coin-summary.input.ts
+++ src/coin-summary/dto/create-coin-summary.input.ts
@@ -4,7 +4,4 @@ import { CoinSummaryInput } from "./coin-summary.input"
 
 @InputType()
 export class CreateCoinSummaryInput extends OmitType(CoinSummaryInput, ["id"]) {
-  @Field({ nullable: false, description: "코인 요약 정보 계좌 번호" })
-  @IsString()
-  accountNumber!: string
 }
